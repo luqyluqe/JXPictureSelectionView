cbd7aa2c91eb8d7bef9223dd66192d3b77968e39
luqyluqe <luqy.luqe@gmail.com>
2016-04-06 15:21:10 +0800

    'add' button

--- Pod/Classes/JXPictureSelectionView.h
+++ Pod/Classes/JXPictureSelectionView.h
@@ -5,10 +5,13 @@
 #import <UIKit/UIKit.h>
 #import "JXPictureSelectionViewConfiguration.h"
 
+typedef void(^JXPictureSelectionViewAddButtonAction)();
+
 @interface JXPictureSelectionView : UIView
 
 @property (strong,nonatomic) JXPictureSelectionViewConfiguration* config;
 @property (strong,nonatomic) NSMutableArray* pictures;
+@property (copy,nonatomic) JXPictureSelectionViewAddButtonAction addButtonAction;
 
 -(instancetype)initWithFrame:(CGRect)frame;
 -(instancetype)initWithFrame:(CGRect)frame configuration:(JXPictureSelectionViewConfiguration*)configuration;
--- Pod/Classes/JXPictureSelectionView.m
+++ Pod/Classes/JXPictureSelectionView.m
@@ -29,8 +29,23 @@
         self.userInteractionEnabled=YES;
         self.pictureWidth=(self.bounds.size.width-self.config.paddingLeft-self.config.paddingRight-self.config.gap*(self.config.numberOfColumns-1))/self.config.numberOfColumns;
         self.pictureHeight=self.pictureWidth/self.config.aspectRatio;
+        self.userInteractionEnabled=YES;
+        [self initAddButton];
     }
     return self;
+}
+
+-(void)initAddButton
+{
+    self.addButton=[[UIView alloc] initWithFrame:CGRectMake(0, 0, self.pictureWidth, self.pictureHeight)];
+    UIImageView* addButtonImageView=[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.config.addButtonImageSize.width, self.config.addButtonImageSize.height)];
+    addButtonImageView.image=self.config.addButtonImage;
+    addButtonImageView.center=CGPointMake(self.addButton.bounds.origin.x+self.pictureWidth/2, self.addButton.bounds.origin.y+self.pictureHeight/2);
+    [self.addButton addSubview:addButtonImageView];
+    UITapGestureRecognizer* tapRecognizer=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(addButtonTapped:)];
+    [self.addButton addGestureRecognizer:tapRecognizer];
+    [self addSubview:self.addButton];
+    [self layoutAddButton];
 }
 
 -(void)addPicture:(UIImage*)picture
@@ -57,6 +72,7 @@
     removeButton.tag=tag;
     [removeButton addTarget:self action:@selector(removeButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:removeButton];
+    [self layoutAddButton];
 }
 
 -(void)removeButtonPressed:(UIButton*)sender
@@ -73,23 +89,28 @@
 
 -(void)removePictureAtIndex:(NSUInteger)index
 {
-    if (index<self.pictures.count) {
-        [self.pictures removeObjectAtIndex:index];
-        for (UIView* view in self.subviews) {
-            if (view.tag==index) {
-                [view removeFromSuperview];
-            }else if (view.tag>index){
-                view.tag-=1;
-                CGPoint position=[self positionOfIndex:view.tag];
-                if ([view isKindOfClass:[UIImageView class]]) {
-                    [UIView animateWithDuration:0.4 animations:^{
-                        view.center=position;
-                    }];
-                }else if ([view isKindOfClass:[UIButton class]]) {
-                    [UIView animateWithDuration:0.4 animations:^{
-                        view.center=CGPointMake(position.x+self.pictureWidth/2, position.y-self.pictureHeight/2);
-                    }];
-                }
+    if (index>=self.pictures.count) {
+        return;
+    }
+    [self.pictures removeObjectAtIndex:index];
+    for (UIView* view in self.subviews) {
+        if (view==self.addButton) {
+            [UIView animateWithDuration:0.4 animations:^{
+                [self layoutAddButton];
+            }];
+        }else if (view.tag==index) {
+            [view removeFromSuperview];
+        }else if (view.tag>index){
+            view.tag-=1;
+            CGPoint position=[self positionOfIndex:view.tag];
+            if ([view isKindOfClass:[UIImageView class]]) {
+                [UIView animateWithDuration:0.4 animations:^{
+                    view.center=position;
+                }];
+            }else if ([view isKindOfClass:[UIButton class]]) {
+                [UIView animateWithDuration:0.4 animations:^{
+                    view.center=CGPointMake(position.x+self.pictureWidth/2, position.y-self.pictureHeight/2);
+                }];
             }
         }
     }
@@ -105,6 +126,16 @@
 
 -(void)layoutAddButton
 {
+    NSUInteger index=self.pictures.count;
+    CGPoint position=[self positionOfIndex:index];
+    self.addButton.center=position;
+}
+
+-(void)addButtonTapped:(UIButton*)button
+{
+    if (self.addButtonAction) {
+        self.addButtonAction();
+    }
 }
 
 @end
